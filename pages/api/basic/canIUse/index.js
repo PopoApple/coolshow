1a88a6755aa0652783b315e481f30b8359038c35
popo <7724401@163.com>
2019-03-10 16:45:17 +0800

    新建

---
+++ pages/api/basic/canIUse/index.js
@@ -0,0 +1,68 @@
+// pages/api/basic/canIUse/index.js
+Page({
+  keys: ['openBluetoothAdapter', 'showShareMenu', 'requestPayment'],
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    results: [],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({ results: this.keys.map(key => {
+      return { key, result: wx.canIUse(key) }
+    }) })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
