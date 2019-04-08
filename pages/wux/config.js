74dce20ab1e6ae5bfa99c924ad83c6d09d767c93
popo <7724401@163.com>
2019-04-08 17:29:32 +0800

    -

---
+++ pages/wux/config.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 'basic',
+    name: '基础组件',
+    open: false,
+    pages: [
+      { id: 'Button', name: 'Button按钮' },
+    ]
+  },
+  {
+    id: 'feedback',
+    name: '操作反馈',
+    open: false,
+    pages: [
+      { id: 'ActionSheet', name: 'ActionSheet上拉菜单' },
+    ]
+  },
+  {
+    id: 'dataView',
+    name: '数据展示',
+    open: false,
+    pages: [
+      { id: 'Badge', name: 'Badge徽章' },
+    ]
+  },
+]
